bd65d7ee3933e16e71a8cadc251454f28ebbdf52
HM Alfee <alfee.personal@gmail.com>
2026-08-11 17:45:44 +0600

    Allow networked access

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
+import os from 'node:os';
+
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {/* config options here */};
+const lanIp =
+    Object.values(os.networkInterfaces())
+        .flat()
+        .find((iface) => iface?.family === 'IPv4' && !iface.internal)
+        ?.address ?? '127.0.0.1';
+
+const nextConfig: NextConfig = {
+    allowedDevOrigins: [lanIp, `${lanIp}.sslip.io`],
+};
 
 export default nextConfig;
